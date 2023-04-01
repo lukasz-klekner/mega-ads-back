96651b12162737f1cedf40a938d843da2ee1983e
Luki Klekner <lukaszklekner@gmail.com>
2023-04-01 11:25:28 +0200

    feat: add prefix to paths

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express, { json, Router } from "express";
 import cors from "cors";
 import "express-async-errors"
 import rateLimit from "express-rate-limit"
@@ -17,7 +17,9 @@ app.use(rateLimit({
     max: 100
 }))
 
-app.use('/ad', adRouter)
+const router = Router()
+router.use('/ad', adRouter)
+app.use('/api', router)
 
 app.use(handleError)
 
